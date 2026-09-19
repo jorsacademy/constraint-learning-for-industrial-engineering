5d7b897649257dd7711729606a10e6df848264b9
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-19 12:39:32 +0300

    Export drift and online adaptation APIs

--- src/industrial_constraint_learning/__init__.py
+++ src/industrial_constraint_learning/__init__.py
@@ -1,16 +1,23 @@
 """Constraint learning tools for industrial engineering examples."""
 
+from .adaptive import AdaptiveConstraintController, AdaptivePolicy, AdaptiveUpdateResult
 from .conformal import ConformalSafetyFilter, SafetyFilterEvaluation
 from .constraint_learner import ManufacturingConstraintLearner
 from .data_generation import generate_manufacturing_data
+from .drift import DistributionShiftMonitor, DriftReport
 from .metrics import BinaryRegionMetrics, evaluate_binary_region
 from .optimization import CandidateOptimizationResult, SafeCandidateOptimizer
 from .tabular import TabularConstraintEvaluation, TabularConstraintLearner
 
 __all__ = [
+    "AdaptiveConstraintController",
+    "AdaptivePolicy",
+    "AdaptiveUpdateResult",
     "BinaryRegionMetrics",
     "CandidateOptimizationResult",
     "ConformalSafetyFilter",
+    "DistributionShiftMonitor",
+    "DriftReport",
     "ManufacturingConstraintLearner",
     "SafeCandidateOptimizer",
     "SafetyFilterEvaluation",
