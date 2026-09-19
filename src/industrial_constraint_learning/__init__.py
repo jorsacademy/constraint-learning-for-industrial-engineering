2841c5f448dac9cbc60af92de5bbef5076a21e48
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-19 12:48:50 +0300

    Export solver embedding APIs

--- src/industrial_constraint_learning/__init__.py
+++ src/industrial_constraint_learning/__init__.py
@@ -6,7 +6,9 @@ from .constraint_learner import ManufacturingConstraintLearner
 from .data_generation import generate_manufacturing_data
 from .drift import DistributionShiftMonitor, DriftReport
 from .metrics import BinaryRegionMetrics, evaluate_binary_region
+from .milp_embedding import LinearConstraintSpec, MILPEmbeddingResult, SurrogateMILPOptimizer
 from .optimization import CandidateOptimizationResult, SafeCandidateOptimizer
+from .surrogate import RiskControlledTreeSurrogate, SurrogateFidelity, sample_uniform_design
 from .tabular import TabularConstraintEvaluation, TabularConstraintLearner
 
 __all__ = [
@@ -18,11 +20,17 @@ __all__ = [
     "ConformalSafetyFilter",
     "DistributionShiftMonitor",
     "DriftReport",
+    "LinearConstraintSpec",
+    "MILPEmbeddingResult",
     "ManufacturingConstraintLearner",
     "SafeCandidateOptimizer",
+    "RiskControlledTreeSurrogate",
     "SafetyFilterEvaluation",
+    "SurrogateFidelity",
+    "SurrogateMILPOptimizer",
     "TabularConstraintEvaluation",
     "TabularConstraintLearner",
     "evaluate_binary_region",
     "generate_manufacturing_data",
+    "sample_uniform_design",
 ]
