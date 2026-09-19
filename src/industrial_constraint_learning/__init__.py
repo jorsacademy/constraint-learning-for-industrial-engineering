d8d5d71077a5046d8146012218ce22cbb4023553
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-19 12:00:03 +0300

    Export safe constraint utilities

--- src/industrial_constraint_learning/__init__.py
+++ src/industrial_constraint_learning/__init__.py
@@ -1,6 +1,15 @@
 """Constraint learning tools for industrial engineering examples."""
 
-from .data_generation import generate_manufacturing_data
 from .constraint_learner import ManufacturingConstraintLearner
+from .data_generation import generate_manufacturing_data
+from .metrics import BinaryRegionMetrics, evaluate_binary_region
+from .optimization import CandidateOptimizationResult, SafeCandidateOptimizer
 
-__all__ = ["generate_manufacturing_data", "ManufacturingConstraintLearner"]
+__all__ = [
+    "BinaryRegionMetrics",
+    "CandidateOptimizationResult",
+    "ManufacturingConstraintLearner",
+    "SafeCandidateOptimizer",
+    "evaluate_binary_region",
+    "generate_manufacturing_data",
+]
