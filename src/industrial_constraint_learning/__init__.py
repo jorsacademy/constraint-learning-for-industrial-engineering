af3dc90fbf93f4e9d95802cb27fa246f64610d8a
Jors Academy <abalci16@posta.pau.edu.tr>
2026-09-19 12:08:12 +0300

    Export generic tabular constraint learner

--- src/industrial_constraint_learning/__init__.py
+++ src/industrial_constraint_learning/__init__.py
@@ -4,12 +4,15 @@ from .constraint_learner import ManufacturingConstraintLearner
 from .data_generation import generate_manufacturing_data
 from .metrics import BinaryRegionMetrics, evaluate_binary_region
 from .optimization import CandidateOptimizationResult, SafeCandidateOptimizer
+from .tabular import TabularConstraintEvaluation, TabularConstraintLearner
 
 __all__ = [
     "BinaryRegionMetrics",
     "CandidateOptimizationResult",
     "ManufacturingConstraintLearner",
     "SafeCandidateOptimizer",
+    "TabularConstraintEvaluation",
+    "TabularConstraintLearner",
     "evaluate_binary_region",
     "generate_manufacturing_data",
 ]
